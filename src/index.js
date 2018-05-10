3702c181ed61b53649762b87fbe04d9277108b10
Rafael Vicio <rafaelvicio@icloud.com>
2018-05-09 21:45:47 -0300

    Get videos

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyC63Xxi26cOYMsA4yt6by1R_VYe6qMbZQY';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: []
+    };
+
+    YTSearch({ key: API_KEY, term: 'react' }, (videos) => {
+      this.setState({ videos });
+      // this.setState({ videos: videos });
+      console.log(videos);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('container'))
